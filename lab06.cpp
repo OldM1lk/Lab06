335141b65fc1c93a3ac9a75ebe97e8ae8762462c
OldM1lk <144899273+OldM1lk@users.noreply.github.com>
2023-10-27 11:20:10 +0700

    Update lab06.cpp

--- lab06.cpp
+++ lab06.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string str;
+  ifstream fin;
+  fin.open("mouse.txt");
+  
+  while ( getline (fin, str) ) {
+    
+  }
+  
+  fin.close();
+  return 0;
+}
